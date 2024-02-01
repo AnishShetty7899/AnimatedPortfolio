402c3c7a3cb9ea94386a60fe53c8186563077d97
Anish Shetty <142916990+AnishShetty7899@users.noreply.github.com>
2024-02-01 18:11:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -122,3 +122,7 @@ function handleScroll() {
     }
   });
 }
+window.addEventListener("scroll", () => {
+  // Use requestAnimationFrame for smoother scrolling
+  requestAnimationFrame(handleScroll);
+});
